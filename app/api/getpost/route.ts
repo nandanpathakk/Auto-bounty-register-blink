fd6db3ce9398681e90a998526a87b30129deb9d7
Nandan Pathak <nandanpathak30@gmail.com>
2024-09-05 15:28:08 +0530

    solving caching issue

--- app/api/getpost/route.ts
+++ app/api/getpost/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Bounty } from "@/db/db";
 
+export const dynamic = 'force-static'
+
 export async function GET(request: NextRequest) {
     const bounties =  await Bounty.find();
     return NextResponse.json(bounties)
